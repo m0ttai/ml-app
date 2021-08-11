2f5d8c1d2f002e6e86af4d89d1d10e9014d602d0
mottai <konakona191@gmail.com>
2021-08-11 12:55:25 +0000

    hotfix

--- app/api/animal_predict_api.py
+++ app/api/animal_predict_api.py
@@ -2,8 +2,6 @@ import io, os
 import json
 from flask import Flask, jsonify, request
 from PIL import Image
-import glob
-from google.cloud.storage import bucket, client
 import numpy as np
 import tensorflow as tf
 from google.cloud import storage as gcs
@@ -14,27 +12,24 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def return_predict():
-	# Variables
-	# gcs_project_id = 'direct-plateau-322502'
+	### Variables ###
 	gcs_project_id = os.environ['GC_PROJECT_ID']
-	# gcs_key_path = '/config/direct-plateau-322502-d65283bd305e.json'
 	gcs_key_path = '/config/gcs_key.json'
-	# gcs_bucket_name = 'ml-app-demo'
 	gcs_bucket_name = os.environ['GCS_BUCKET_NAME']
 	class_label = ('cat', 'crow', 'horse', 'lion', 'turtle')
 	model_path = '/model.h5'
 	model = tf.keras.models.load_model(model_path)
 	X = []
 
-	# Get Request
+	### Get Request ###
 	req = json.loads(request.json)
 
-	# Get Image File
+	### Get Image File ###
 	gcs_client = gcs.Client(gcs_project_id, credentials=service_account.Credentials.from_service_account_file(gcs_key_path))
 	gcs_bucket = gcs_client.get_bucket(gcs_bucket_name)
 	predict_file = gcs_bucket.get_blob(req['file_name'])
 
-	# Retouch & Convert Image File
+	### Retouch & Convert Image File ###
 	input_file = Image.open(io.BytesIO(predict_file.download_as_string()))
 	input_file = input_file.convert('RGB')
 	input_file = input_file.resize((100, 100))
@@ -42,10 +37,10 @@ def return_predict():
 	X.append(input_file)
 	X = np.array(X)
 
-	# Predict Image File
+	### Predict Image File ###
 	results = model.predict([X])[0]
 
-	# Set Object's Metadata
+	### Set Object's Metadata ###
 	gcs_metadata = {'class': class_label[results.argmax()]}
 	predict_file.metadata = gcs_metadata
 	predict_file.patch()
